abfe5f14472db04cccdae84879831fd12739bf31
naila-ady <164941279+naila-ady@users.noreply.github.com>
2024-12-19 20:50:34 +0500

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
-
+import Hero from "@/components/Hero"
+import Jobs from "./jobs/page";
+import Apply from "./apply/page";
 export default function Home() {
   return (
  <div>
   
+  <Hero/>
+  <Jobs/>
+  <Apply/>
+
  </div>
   );
 }
